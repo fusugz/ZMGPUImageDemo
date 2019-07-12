13c1739d620dab11f04633dc85dfbfbbe4675ad2
zheng min <mp@mpdeMacBook-Pro.local>
2019-07-12 13:49:24 +0800

    GPU Image

---
+++ GPUImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImageDemo
+//
+//  Created by zheng min on 2019/5/21.
+//  Copyright © 2019 Micropattern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPUImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImageDemo
+//
+//  Created by zheng min on 2019/5/21.
+//  Copyright © 2019 Micropattern. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
